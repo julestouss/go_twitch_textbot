265d40458eda4c0ab40a6dd4a34c1770d99afd87
jules.touss <150541589+julestouss@users.noreply.github.com>
2025-05-19 18:42:59 +0200

    connecte au compte twitch + reçois les messages - crash lors du traitement d'un message

---
+++ go.mod
@@ -0,0 +1,5 @@
+module twitch_chat_bot
+
+go 1.24.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net"
+	"net/textproto"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+var token, channel, username string
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erreur chargement du .env")
+	}
+
+	token = os.Getenv("TWITCH_OAUTH_TOKEN")
+	username = os.Getenv("TWITCH_USERNAME")
+	channel = os.Getenv("TWITCH_CHAN_NAME")
+
+	if token == "" || username == "" || channel == "" {
+		log.Fatal("Probleme lors de la reception des variables d'environement")
+	}
+}
+
+func connect() net.Conn {
+	conn, err := net.Dial("tcp", "irc.chat.twitch.tv:6667")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Token:", token)
+	fmt.Println("Username:", username)
+	fmt.Println("Channel:", channel)
+
+	fmt.Fprintf(conn, "PASS %s\r\n", token)
+	fmt.Fprintf(conn, "NICK %s\r\n", username)
+	fmt.Fprintf(conn, "JOIN #%s\r\n", channel)
+
+	return conn
+}
+
+func disconnect(conn net.Conn) {
+	conn.Close()
+}
+
+func sendMessage(conn net.Conn, message string) {
+	fmt.Fprintf(conn, "PRIVMSG #%s :%s\r\n", channel, message)
+}
+
+func processMSG(conn net.Conn, message string) {
+	type messageHandle struct {
+		cmd string
+		msg string
+	}
+
+	file, err := os.Open("dic.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 2
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	for i, line := range data {
+		if i > 0 {
+			var msgH messageHandle
+			for j, field := range line {
+				if j == 0 {
+					msgH.cmd = field
+				}
+				if j == 1 {
+					msgH.msg = field
+				}
+				if message == msgH.cmd {
+					sendMessage(conn, msgH.msg)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	log.Println("Debut du programme")
+
+	conn := connect()
+	defer disconnect(conn)
+
+	tp := textproto.NewReader(bufio.NewReader(conn))
+	for {
+		status, err := tp.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(status)
+
+		if strings.Contains(status, "PRIVMSG") {
+			messageParts := strings.Split(status, " :")
+			if len(messageParts) > 1 {
+				message := messageParts[1]
+				processMSG(conn, message)
+			}
+		}
+	}
+
+}
